1b332f47cfd986e74b5ab8b678eb1da888fc59c6
mateen kazia <MateenCode@gmail.com>
2020-11-24 12:55:22 -0800

    Update ClassComponent.js

--- src/components/ClassComponent.js
+++ src/components/ClassComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export class ClassComponent extends Component {
+class ClassComponent extends Component {
     render() {
         return (
             <div>

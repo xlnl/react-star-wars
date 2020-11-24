df5ed623122f4beabf08afa07029f63bd10756e7
mateen kazia <MateenCode@gmail.com>
2020-11-24 13:07:35 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import './css/App.css';
-import FunctionalComponent from './components/FunctionalComponent'
-import ClassComponent from './components/ClassComponent'
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <FunctionalComponent />
-      <ClassComponent />
-    </div>
-  );
+import FunctionalComponent from "./components/FunctionalComponent";
+import ClassComponent from "./components/ClassComponent";
+
+import "./css/App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <FunctionalComponent />
+        <ClassComponent />
+      </div>
+    );
+  }
 }
 
 export default App;
